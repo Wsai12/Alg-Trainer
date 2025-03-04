f48ae7ffacce3d2c35b68f6aa284665286c15480
Wsai12 <williamhuang1011@gmail.com>
2025-03-03 16:49:50 -0800

    Revert "Revert "improved hint system""
    This reverts commit d025539478a9abdca669fa17749676d0da832eb6.

--- js/hintSystem.js
+++ js/hintSystem.js
@@ -1,19 +1,18 @@
-// filepath: Alg-Trainer/js/hintSystem.js
 document.getElementById('hintButton').addEventListener('click', showHint);
 
 function showHint() {
     const algorithm = "U R U' R'"; // Example algorithm, replace with actual algorithm
     const nextMove = getNextMove(algorithm);
     const plan = getPlan(algorithm);
-    alert(`Next Move: ${nextMove}\nPlan: ${plan}`);
+    document.getElementById('hintdisp').innerText = `Next Move: ${nextMove}\nPlan: ${plan}`;
 }
 
-function getNextMove() {
+function getNextMove(algorithm) {
     // Implement logic to get the next move of the algorithm
-    return "U";
+    return algorithm.split(" ")[0];
 }
 
-function getPlan() {
+function getPlan(algorithm) {
     // Implement logic to get the plan for the algorithm
     return "Solve the cross";
 }
