0f9d7b2b7f16726268880a6bbffed30005a05f3d
Wsai12 <williamhuang1011@gmail.com>
2025-03-03 16:38:42 -0800

    Revert "please be fixed"
    This reverts commit abf91e06c2a09f13cc3ea81209606a801cccf888.

--- js/hintSystem.js
+++ js/hintSystem.js
@@ -1,11 +1,18 @@
-document.getElementById('hintButton').addEventListener('click', () => showHint("U R U' R'"));
+document.getElementById('hintButton').addEventListener('click', showHint);
 
-function showHint(algorithm) {
+function showHint() {
+    const algorithm = "U R U' R'"; // Example algorithm, replace with actual algorithm
     const nextMove = getNextMove(algorithm);
-    document.getElementById('hintdisp').innerText = `Next Move: ${nextMove}`;
+    const plan = getPlan(algorithm);
+    document.getElementById('hintdisp').innerText = `Next Move: ${nextMove}\nPlan: ${plan}`;
 }
 
 function getNextMove(algorithm) {
     // Implement logic to get the next move of the algorithm
     return algorithm.split(" ")[0];
+}
+
+function getPlan(algorithm) {
+    // Implement logic to get the plan for the algorithm
+    return "Solve the cross";
 }
